696d0de0ee075a03a4326c45edd333fb639bdb75
kibe0218 <kibe0218@gmail.com>
2025-10-10 20:37:12 +0900

    Initial Commit

---
+++ eitango/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  eitango
+//
+//  Created by kibe on 2025/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("fa, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eitango/eitangoApp.swift
@@ -0,0 +1,17 @@
+//
+//  eitangoApp.swift
+//  eitango
+//
+//  Created by kibe on 2025/10/10.
+//
+
+import SwiftUI
+
+@main
+struct eitangoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
