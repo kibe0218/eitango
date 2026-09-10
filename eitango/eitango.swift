809fe388f4edd730e6838410d5a499ad137e064f
kibe0218 <kibe0218@gmail.com>
2026-09-10 15:08:01 +0900

    Group削除

--- eitango/eitango.swift
+++ eitango/eitango.swift
@@ -12,26 +12,19 @@ struct eitangoApp: App {
     
     var body: some Scene {
         WindowGroup {
-            Group {
-                if vm.userSession.user == nil {
-                    NavigationStack(path: $vm.authPath) {
-                        LogInView()
-                            .navigationDestination(for: AuthScreen.self) { screen in
-                                switch screen {
-                                case .signUp:
-                                    SignUpView()
-                                }
+            if vm.userSession.user == nil {
+                NavigationStack(path: $vm.authPath) {
+                    LogInView()
+                        .navigationDestination(for: AuthScreen.self) { screen in
+                            switch screen {
+                            case .signUp:
+                                SignUpView()
                             }
-                            .environmentObject(vm)
-                    }
-                } else {
-                    SplashScreenView()
+                        }
+                        .environmentObject(vm)
                 }
-            }
-            .onAppear {
-                print("🟡 RootView表示された")
-                print("🟡 user:", vm.userSession.user as Any)
-                vm.userActions.fetch()
+            } else {
+                HomeView()
             }
         }
         .environmentObject(vm)
